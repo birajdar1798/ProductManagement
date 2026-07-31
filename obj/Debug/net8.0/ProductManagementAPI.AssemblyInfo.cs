96cdcf1394ed45e14e449b65e59c367a5726b269
Siddarth Birajdar <siddarth.birajdar@eversana.com>
2026-07-31 12:24:09 +0530

    test

--- obj/Debug/net8.0/ProductManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d36c0e7f6b5ec97d61643fd2b1e32f4153b15b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349b94c379cf51f3ee64fb04a99b0980fd32bdbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
