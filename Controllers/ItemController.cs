d7c4a61874f9f5506acecbeff3a9d95086812776
Soham Bawaskar <soham.bawaskar@Eversana.com>
2026-08-13 13:12:16 +0530

    Addded Delete Method

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -54,8 +54,25 @@ namespace ProductManagementAPI.Controllers
                 return Ok(false);
             }
 
+        }
 
+        [HttpDelete]
 
+        public IActionResult DeleteItem([FromQuery] int id)
+        {
+
+            var item = _context.Item.FirstOrDefault(item => item.Id == id);
+
+            if (item == null)
+            {
+               return NotFound("Item with id do not exists.");
+            }
+            else
+            {
+                _context.Item.Remove(item);
+                _context.SaveChanges();
+                return Ok("Item Deleted Successfully");
+            }
         }
     }
 }
